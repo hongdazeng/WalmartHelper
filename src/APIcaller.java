4b6d1741c73ba60fafa0d41e8619d584cb2a9690
FireSoul1 <gkondapa@purdue.edu>
2015-10-17 19:07:43 -0400

    Adding in Parsing Functions

--- src/APIcaller.java
+++ src/APIcaller.java
@@ -1,8 +1,14 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.StringTokenizer;
+
+import javax.imageio.ImageIO;
 
 /**
  * Created by Hongda Zeng on 10/17/2015!
@@ -47,11 +53,11 @@ public class APIcaller {
 
         return response;
     }
-
+    
     public double getUPC() throws IOException {
 
         String callResponse = doCall();
-
+    
         int start = callResponse.indexOf("upc");
         start += 6; //offset to start of upc
         String upcStr = callResponse.substring(start, start + 12);
@@ -59,6 +65,39 @@ public class APIcaller {
 
         return upc;
     }
-
+    public String getName() throws IOException
+    {
+        String str = doCall();
+        int indName = str.indexOf("name");
+        StringTokenizer par = new StringTokenizer(str.substring(indName),",");
+		//System.out.println("The Name: "+par.nextToken().split(":")[1].replace("\"", ""));
+		return par.nextToken().split(":")[1].replace("\"", "");
+    }
+    
+    public double getPrice() throws Exception
+    {
+        String str = doCall();
+        int indPrice = str.indexOf("salePrice");
+        //next is the sales price
+		StringTokenizer par = new StringTokenizer(str.substring(indPrice),",");
+		//System.out.println("The Price: $"+par.nextToken().split(":")[1].replace("\"", ""));
+		return Double.parseDouble(par.nextToken().split(":")[1].replace("\"", ""));
+			
+    }
+    
+    public File getImage(String name)throws Exception
+    {
+        String str = doCall();
+        int indImage = str.indexOf("Image");
+    	StringTokenizer par = new StringTokenizer(str.substring(indImage),",");
+		String outImage = par.nextToken().split(":")[2].replace("\"", "").replace("//", "");
+		///write the file 
+		URL im = new URL("http://"+outImage);
+		Image ou = ImageIO.read(im);
+		BufferedImage fin = (BufferedImage) ou;
+		File outputfile = new File(name);
+		ImageIO.write(fin, "png", outputfile);
+		return outputfile;
+    }
 
 }
